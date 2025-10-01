6fc00881ce965d2ef16f3578e3b1fae5dfebfeeb
pshlego <parksh0712@postech.ac.kr>
2025-10-01 15:39:53 +0900

    add opadvisor

---
+++ source/tuning/OpAdviserPrivate/OpAdviserPlus/space.py
@@ -0,0 +1,100 @@
+from collections import defaultdict
+import json
+import pprint
+import re
+from typing import List
+
+import pandas as pd
+
+
+class Hyperparameter:
+    def __init__(
+        self, name: str, param_type: str, param_range: List[int], default: int
+    ):
+        self.name = name
+        self.param_type = param_type
+        self.param_range = param_range
+        self.default = default
+
+    def __repr__(self) -> str:
+        return (
+            f"Hyperparameter(name='{self.name}', type='{self.param_type}', "
+            f"range={self.param_range}, default={self.default})"
+        )
+
+
+CONFIG_BLOCK_PATTERN = re.compile(
+    r"Configuration space object:\s*"
+    r"Hyperparameters:(.*?)(?=Configuration space object:|$)",
+    re.DOTALL,  # Enables matching across multiple lines
+)
+HYPERPARAMETER_PATTERN = re.compile(
+    r"\s*(\w+), Type: (\w+), Range: \[(\d+), (\d+)\], Default: (\d+)"
+)
+if __name__ == "__main__":
+    for s in [
+        # "oltpbench_tatp",
+        # "oltpbench_tpcc",
+        # "oltpbench_wikipedia",
+        # "oltpbench_ycsb",
+        # "sbread",
+        "sbrw",
+        # "sbwrite",
+        # "twitter",
+    ]:
+        with open(f"repo_backup/history_{s}_smac.json") as f:
+            j = json.load(f)["data"]
+            c = sorted(j, key=lambda x: x["external_metrics"].get("tps", 0))[-1]
+            pprint.pprint((
+                # c["configuration"], 
+                c["external_metrics"]))
+        with open(f"repo_backup/history_{s}_ground_truth.json") as f:
+            j = json.load(f)["data"]
+            c = sorted(j, key=lambda x: x["external_metrics"].get("tps", 0))[-1]
+            pprint.pprint((
+                # c["configuration"], 
+                           c["external_metrics"]))
+        c = c["configuration"]
+        l = []
+        with open(f"scripts/experiment/gen_knobs/mysql_all_197_32G.json") as f:
+            j2 = json.load(f)
+            for k in j2:
+                if j2[k]["type"] == "enum":
+                    if c[k] == j2[k]["default"]:
+                        l.append(1)
+                    else:
+                        l.append(0)
+        print(sum(l)/len(l))
+        with open(f"logs/DBTune-{s}_smac.log") as f:
+            config_text = f.read()
+            config_blocks = CONFIG_BLOCK_PATTERN.findall(config_text)
+            d = defaultdict(list)
+            for block in config_blocks:
+                hyperparameters = []
+                for match in HYPERPARAMETER_PATTERN.findall(block):
+                    name, param_type, range_start, range_end, default = match
+                    if param_type != "UniformInteger":
+                        pprint.pprint(param_type)
+                    if int(range_start) <= c[name] <= int(range_end):
+                        d[name].append(1)
+                    else:
+                        d[name].append(0)
+                    hyperparameters.append(
+                        Hyperparameter(
+                            name=name,
+                            param_type=param_type,
+                            param_range=[int(range_start), int(range_end)],
+                            default=int(default),
+                        )
+                    )
+                # pprint.pprint(hyperparameters)
+        pd.DataFrame(
+            map(lambda x: (x[0], len(x[1]), sum(x[1]) / len(x[1])), d.items()),
+            columns=["key", "times", "proportion"],
+        ).to_csv(f"{s}.csv", index=False)
+        # print(d)
+        # pprint.pprint(
+        #     list(
+        #         map(lambda x: (x[0], x[1]["external_metrics"].get("tps", 0)), enumerate(j))
+        #     )
+        # )
---
+++ source/tuning/OpAdviserPrivate/server/app.py
@@ -0,0 +1,173 @@
+import argparse
+import pprint
+from datetime import timedelta
+
+import mysql
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from autotune.database.mysqldb import MysqlDB
+from autotune.database.postgresqldb import PostgresqlDB
+from autotune.dbenv import DBEnv
+from autotune.tuner import DBTuner
+from autotune.utils.config import parse_args
+
+dbconfig = {
+    "user": "root",
+    "password": "password",
+    "database": "concert_singer",
+    "unix_socket": "/var/run/mysqld/mysqld.sock",
+}
+while True:
+    try:
+        mydb = mysql.connector.connect(**dbconfig)
+        cursor = mydb.cursor()
+        cursor.execute(
+            """
+SHOW VARIABLES 
+where variable_name!='optimizer_switch'
+and variable_name!='sql_mode'
+and variable_name!='session_track_system_variables';
+"""
+        )
+        data = cursor.fetchall()
+        pprint.pprint(data)
+        break
+    except Exception as e:
+        print(e)
+    finally:
+        if "cursor" in locals():
+            cursor.close()
+        if "mydb" in locals():
+            mydb.close()
+app = Flask(__name__)
+CORS(app)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--config", type=str, default="scripts/demo.ini", help="config file"
+)
+parser.add_argument("run")
+parser.add_argument("--no-debugger", action="store_true")
+parser.add_argument("--no-reload", action="store_true")
+opt = parser.parse_args()
+
+
+args_db, args_tune = parse_args(opt.config)
+if args_db["db"] == "mysql":
+    db = MysqlDB(args_db)
+elif args_db["db"] == "postgresql":
+    db = PostgresqlDB(args_db)
+
+env = DBEnv(args_db, args_tune, db)
+tuner = DBTuner(args_db, args_tune, env)
+queries = list()
+times = list()
+
+
+@app.post("/query")
+def query():
+    query = request.get_json()
+    if query == "conduct tuning":
+        with open(
+            "/workspaces/OpAdviserPrivate/autotune/cli/selectedList_demo.txt", "w"
+        ) as _:
+            pass
+        for i, q in enumerate(queries):
+            with open(
+                f"/workspaces/OpAdviserPrivate/autotune/demo_query/queries-mysql-new/{i}.sql",
+                "w",
+            ) as f1:
+                f1.write(
+                    f"""
+select current_timestamp(6) into @query_start;
+set @query_name='{i}';
+{q};
+set @query_time_ms= timestampdiff(microsecond, @query_start, current_timestamp(6))/1000;
+SELECT @query_name, @query_time_ms;
+"""
+                )
+            with open(
+                "/workspaces/OpAdviserPrivate/autotune/cli/selectedList_demo.txt", "a"
+            ) as f2:
+                f2.write(
+                    f"""{i}.sql
+"""
+                )
+        while True:
+            tuner.tune()
+            try:
+                mydb = mysql.connector.connect(**dbconfig)
+                execution_times = []
+                for q in queries:
+                    cursor = mydb.cursor(dictionary=True)
+                    cursor.execute("""select current_timestamp(6);""")
+                    start_time = cursor.fetchall()[0]["current_timestamp(6)"]
+                    cursor.execute(q)
+                    cursor.fetchall()
+                    cursor.execute("""select current_timestamp(6);""")
+                    end_time = cursor.fetchall()[0]["current_timestamp(6)"]
+                    execution_times.append(
+                        (end_time - start_time) / timedelta(milliseconds=1)
+                    )
+                b = True
+                for i in range(len(times)):
+                    if execution_times[i] > times[i]:
+                        b = False
+                if b:
+                    return {
+                        "execution_times": execution_times,
+                    }
+            finally:
+                if "cursor" in locals():
+                    cursor.close()
+                if "mydb" in locals():
+                    mydb.close()
+    try:
+        queries.append(query)
+        mydb = mysql.connector.connect(**dbconfig)
+        cursor = mydb.cursor(dictionary=True)
+        cursor.execute("""select current_timestamp(6);""")
+        start_time = cursor.fetchall()[0]["current_timestamp(6)"]
+        cursor.execute(query)
+        data = cursor.fetchall()
+        cursor.execute("""select current_timestamp(6);""")
+        end_time = cursor.fetchall()[0]["current_timestamp(6)"]
+        execution_time = (end_time - start_time) / timedelta(milliseconds=1)
+        times.append(execution_time)
+        return {
+            "data": data[:10],
+            "execution_time": execution_time,
+        }
+    finally:
+        if "cursor" in locals():
+            cursor.close()
+        if "mydb" in locals():
+            mydb.close()
+
+
+@app.post("/knobs")
+def knobs():
+    try:
+        mydb = mysql.connector.connect(**dbconfig)
+        cursor = mydb.cursor()
+        cursor.execute(
+            """
+SHOW VARIABLES 
+where variable_name!='optimizer_switch'
+and variable_name!='sql_mode'
+and variable_name!='session_track_system_variables';
+"""
+        )
+        data = cursor.fetchall()
+        return jsonify(data)
+    finally:
+        if "cursor" in locals():
+            cursor.close()
+        if "mydb" in locals():
+            mydb.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=False, use_reloader=False, port=1234)
